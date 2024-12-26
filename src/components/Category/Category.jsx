0765dddeefeca43b90784a45c696a7200553e021
Dayveesptf <dtechservices2@gmail.com>
2024-12-26 18:05:28 +0100

    End

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -5,11 +5,11 @@ const Category = ({onSelectCategory, activeCategory}) => {
 
   return (
     <div className='px-4 mb-8 lg:space-x-16  flex flex-wrap items-center border-b-2 py-5 text-gray-900 font-semibold'>
-        <button onClick={() => onSelectCategory(null)} className={`lg:ml-12 ${activeCategory ? "" : "active-button"}`}>All</button>
+        <button onClick={() => onSelectCategory(null)} className={`lg:ml-12 small-device ${activeCategory ? "" : "active-button"}`}>All</button>
         {
             categories.map((category) => {
                 return (
-                    <button onClick={() => onSelectCategory(category)} className={`mr-2 space-x-16 ml-3${activeCategory === category ? "active-button" : ""}`} key={category}> {category} </button>
+                    <button onClick={() => onSelectCategory(category)} className={`mr-2 space-x-16 small-device ml-3${activeCategory === category ? "active-button" : ""}`} key={category}> {category} </button>
                 );
             })
         }
